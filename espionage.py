d386c3bbe8359ec22c606db91e67c0710dc684ed
inaz2 <inaz2@twitter>
2014-02-27 16:23:11 +0900

    espionage.py: implement following_protected & dump_tweets

--- espionage.py
+++ espionage.py
@@ -70,7 +70,7 @@ def sources(args):
     screen_name = args[0]
 
     api = get_api()
-    tweets = api.user_timeline(screen_name=screen_name, count=100, include_rts=False)
+    tweets = api.user_timeline(screen_name=screen_name, count=200, include_rts=False)
     ctr = Counter(t.source for t in tweets)
 
     for source, count in ctr.most_common():
@@ -84,23 +84,67 @@ def diff_following(args):
     screen_name, exclude_screen_names = args[0], args[1:]
 
     api = get_api()
-    friends = api.friends_ids(screen_name=screen_name)
-    friends = set(friends)
+    f_ids = api.friends_ids(screen_name=screen_name)
+    f_ids = set(f_ids)
 
     for sname in exclude_screen_names:
-        exclude_friends = api.friends_ids(screen_name=sname)
-        friends -= set(exclude_friends)
+        exclude_f_ids = api.friends_ids(screen_name=sname)
+        f_ids -= set(exclude_f_ids)
 
-    friends = list(friends)
-    friends = api.lookup_users(user_ids=friends)
+    f_ids = list(f_ids)
+    friends = []
+    for i in xrange(len(f_ids)/100+1):
+        ids = f_ids[100*i:100*(i+1)]
+        users = api.lookup_users(user_ids=ids)
+        friends.extend(users)
 
     for user in friends:
         print "%s\t%s\t\x1b[1;30m%s\x1b[0m" % (user.screen_name, user.name, user.description.replace('\n', ' '))
 
 
+def following_protected(args):
+    if len(args) < 1:
+        print >>sys.stderr, "Usage: python %s following_protected SCREEN_NAME" % sys.argv[0]
+        sys.exit(1)
+    screen_name = args[0]
+
+    api = get_api()
+    f_ids = api.friends_ids(screen_name=screen_name)
+    friends = []
+    for i in xrange(len(f_ids)/100+1):
+        ids = f_ids[100*i:100*(i+1)]
+        users = api.lookup_users(user_ids=ids)
+        friends.extend(users)
+
+    for user in friends:
+        if not user.protected:
+            continue
+        print "%s\t%s\t\x1b[1;30m%s\x1b[0m" % (user.screen_name, user.name, user.description.replace('\n', ' '))
+
+
+def dump_tweets(args):
+    if len(args) < 1:
+        print >>sys.stderr, """Usage: python %s dump_tweets SCREEN_NAME
+
+Note: You can dump only recent 3200 tweets.""" % sys.argv[0]
+        sys.exit(1)
+    screen_name = args[0]
+
+    api = get_api()
+    cursor = tweepy.cursor.Cursor(api.user_timeline, screen_name=screen_name, count=200, include_rts=True)
+    for t in cursor.items():
+        t.created_at += timedelta(hours=+9)
+        if getattr(t, 'in_reply_to_status_id', None):
+            print "[%s] \x1b[1;30m%s: %s\x1b[0m" % (t.created_at, t.user.screen_name, t.text.replace('\n', ' ').replace('&lt;', '<').replace('&gt;', '>'))
+        elif getattr(t, 'retweeted_status', None):
+            print "[%s] \x1b[32m%s: %s\x1b[0m" % (t.created_at, t.user.screen_name, t.text.replace('\n', ' ').replace('&lt;', '<').replace('&gt;', '>'))
+        else:
+            print "[%s] \x1b[37m%s: %s\x1b[0m" % (t.created_at, t.user.screen_name, t.text.replace('\n', ' ').replace('&lt;', '<').replace('&gt;', '>'))
+
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print >>sys.stderr, "Usage: python %s [timeline|fans|sources|diff_following] ..." % sys.argv[0]
+        print >>sys.stderr, "Usage: python %s [timeline|fans|sources|diff_following|following_protected|dump_tweets] ..." % sys.argv[0]
         sys.exit(1)
     subcommand, args = sys.argv[1], sys.argv[2:]
 
@@ -112,6 +156,10 @@ if __name__ == '__main__':
         sources(args)
     elif subcommand == 'diff_following':
         diff_following(args)
+    elif subcommand == 'following_protected':
+        following_protected(args)
+    elif subcommand == 'dump_tweets':
+        dump_tweets(args)
     else:
-        print >>sys.stderr, "Usage: python %s [timeline|fans|sources|diff_following] ..." % sys.argv[0]
+        print >>sys.stderr, "Usage: python %s [timeline|fans|sources|diff_following|following_protected|dump_tweets] ..." % sys.argv[0]
         sys.exit(1)
